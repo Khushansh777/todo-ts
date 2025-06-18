4b43e046c850e87069dc84fa6a4815b78c39d001
Khushansh <khushanshbaliyan@gmail.com>
2025-06-18 13:02:33 +0530

    made edit functionality

--- dist/todo.js
+++ dist/todo.js
@@ -61,9 +61,10 @@ function renderToDo() {
     array.forEach((element) => {
         html += `<li class="todo-item">
                 <input type="checkbox" class="todo-checkbox">
+                <input type='text' class="edit-input displayNone" value="${element.name}">
                 <span class="todo-text">${element.name}</span>
                 <div class="todo-actions">
-                    <button class="edit-button">
+                    <button class="edit-button" data-edit-btn-id="${element.id}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                             <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
@@ -89,21 +90,65 @@ function resetInput() {
     submitForm.value = '';
 }
 // Delete Btn functionality
-todoContainer.addEventListener('click', (event) => {
-    const target = event.target;
+// todoContainer.addEventListener('click', (event) => {
+//     const target = event.target as HTMLElement
+//     const deleteBtn = target.closest('.delete-button') as HTMLButtonElement
+//     if(deleteBtn){
+//         console.log('deleteBTn',deleteBtn);
+//         const datasetId = deleteBtn.dataset.deleteBtnId;
+//         // find the matched item and updates array
+//         array = array.filter(element => element.id.toString() !== datasetId);
+//         //store array
+//         storeTodo(array)
+//         //re-render TODO
+//         renderToDo()
+//     }
+//     // const deleteBtnID:number = parseInt(deleteBtn.dataset.id);
+//     // console.log(deleteBtnID)
+// });
+//edit button functionality
+todoContainer.addEventListener('click', (e) => {
+    var _a, _b, _c;
+    const target = e.target;
+    // handles delete Btn
     const deleteBtn = target.closest('.delete-button');
     if (deleteBtn) {
         console.log('deleteBTn', deleteBtn);
-        const datasetId = deleteBtn.dataset.deleteBtnIgotd;
+        const datasetId = deleteBtn.dataset.deleteBtnId;
         // find the matched item and updates array
         array = array.filter(element => element.id.toString() !== datasetId);
         //store array
         storeTodo(array);
         //re-render TODO
         renderToDo();
+        return;
     }
-    // const deleteBtnID:number = parseInt(deleteBtn.dataset.id);
-    // console.log(deleteBtnID)
+    // habdles edit button
+    const editButton = target.closest('.edit-button');
+    if (editButton) {
+        // find the matched item and updates array
+        const datasetId = (_a = editButton.dataset.editBtnId) === null || _a === void 0 ? void 0 : _a.toString();
+        const matched = array.find(element => element.id.toString() === datasetId);
+        console.log(matched);
+        if (matched) {
+            // get values if there is a matched item
+            const todoItem = editButton.closest('.todo-item');
+            const todoText = todoItem.querySelector('.todo-text');
+            const inputVal = todoItem.querySelector('.edit-input');
+            if (((_b = editButton.textContent) === null || _b === void 0 ? void 0 : _b.trim()) === 'Edit') {
+                // Save updated TODO and Handles UI accordingly
+                inputVal.classList.remove('displayNone');
+                inputVal.value = matched.name;
+                todoText.classList.add('displayNone');
+                editButton.innerText = 'Done';
+            }
+            else if (((_c = editButton.textContent) === null || _c === void 0 ? void 0 : _c.trim()) === 'Done') {
+                matched.name = inputVal.value;
+                storeTodo(array);
+                renderToDo();
+            }
+        }
+    }
 });
 // run function when btn click
 submitbutton.addEventListener('click', () => {
--- src/scripts/todo.ts
+++ src/scripts/todo.ts
@@ -81,9 +81,10 @@ function renderToDo():void{
        
     html += `<li class="todo-item">
                 <input type="checkbox" class="todo-checkbox">
+                <input type='text' class="edit-input displayNone" value="${element.name}">
                 <span class="todo-text">${element.name}</span>
                 <div class="todo-actions">
-                    <button class="edit-button">
+                    <button class="edit-button" data-edit-btn-id="${element.id}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                             <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
@@ -110,24 +111,76 @@ function resetInput(){
  }
 
 // Delete Btn functionality
-todoContainer.addEventListener('click', (event) => {
-    const target = event.target as HTMLElement
-    const deleteBtn = target.closest('.delete-button') as HTMLButtonElement
+// todoContainer.addEventListener('click', (event) => {
+//     const target = event.target as HTMLElement
+//     const deleteBtn = target.closest('.delete-button') as HTMLButtonElement
+
+//     if(deleteBtn){
+//         console.log('deleteBTn',deleteBtn);
+//         const datasetId = deleteBtn.dataset.deleteBtnId;
+//         // find the matched item and updates array
+//         array = array.filter(element => element.id.toString() !== datasetId);
+//         //store array
+//         storeTodo(array)
+//         //re-render TODO
+//         renderToDo()
+//     }
+//     // const deleteBtnID:number = parseInt(deleteBtn.dataset.id);
+//     // console.log(deleteBtnID)
+// });
+
+//edit button functionality
+todoContainer.addEventListener('click',(e) =>{
+    const target  = e.target as HTMLElement
+
+
+// handles delete Btn
+     const deleteBtn = target.closest('.delete-button') as HTMLButtonElement
 
     if(deleteBtn){
         console.log('deleteBTn',deleteBtn);
-        const datasetId = deleteBtn.dataset.deleteBtnIgotd;
+        const datasetId = deleteBtn.dataset.deleteBtnId;
         // find the matched item and updates array
         array = array.filter(element => element.id.toString() !== datasetId);
         //store array
         storeTodo(array)
         //re-render TODO
         renderToDo()
+        return
     }
-    // const deleteBtnID:number = parseInt(deleteBtn.dataset.id);
-    // console.log(deleteBtnID)
-});
+// habdles edit button
 
+    const editButton =  target.closest('.edit-button') as HTMLElement;
+   
+    if(editButton){
+// find the matched item and updates array
+    const datasetId = editButton.dataset.editBtnId?.toString();
+  
+    const matched = array.find(element => element.id.toString() === datasetId);
+    console.log(matched)
+    if(matched){
+// get values if there is a matched item
+       const todoItem = editButton.closest('.todo-item') as HTMLElement;
+            const todoText = todoItem.querySelector('.todo-text') as HTMLElement;
+            const inputVal = todoItem.querySelector('.edit-input') as HTMLInputElement;
+     
+      if(editButton.textContent?.trim() === 'Edit'){
+// Save updated TODO and Handles UI accordingly
+        
+        inputVal.classList.remove('displayNone');
+        inputVal.value = matched.name ;
+        todoText.classList.add('displayNone');
+        editButton.innerText = 'Done';
+      }
+      else if(editButton.textContent?.trim() === 'Done'){
+         matched.name = inputVal.value;
+         storeTodo(array);
+         renderToDo()
+       
+    }
+    }
+    }
+})
 // run function when btn click
 submitbutton.addEventListener('click', () =>{
      getValueofInput();
